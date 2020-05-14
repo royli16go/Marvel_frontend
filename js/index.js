aaeb879b9bd9dd50abadfd99db4549eef336b14c
royli16go <royli16go@gmail.com>
2020-05-14 16:10:07 +0800

    create index.html

---
+++ js/index.js
@@ -0,0 +1,47 @@
+$token = localStorage.getItem("token");
+if($token){
+    $('#Register').hide();
+    $('#Login').hide();
+    $('#Logout').show();
+} else {
+    $('#Register').show();
+    $('#Login').show();
+    $('#Logout').hide();
+}
+
+$("#BtnLogin").click(function() {
+    var userdata = {
+        "email":$("#email").val(),
+        "password":$('#password').val()
+    };
+    var JSON_userdata = JSON.stringify(userdata);
+    console.log(JSON_userdata);
+    $.ajax({
+        url: 'http://localhost:3000/api/user/login',
+        type: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: JSON_userdata,
+        dataType: 'JSON',
+        success: function (result) {
+            console.log(result['message']);
+            alert(result['message']);
+            console.log(result['token']);
+            localStorage.setItem("token", result['token']);  
+            location.href='/frontend/welcome.html';
+        },
+        error: function(result){
+            console.log(result['responseText']);
+            alert(result['responseText']);
+        }
+      });
+});
+
+$('#Logout').click(function() {
+    localStorage.removeItem('token');
+    $('#Register').show();
+    $('#Login').show();
+    $('#Logout').hide();
+    location.href='/frontend/index.html';
+});
